ca057b534dccc157b81216b35be2822044f506f9
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2017-04-08 15:42:33 +0100

    Create check battery charge

---
+++ check-battery-charge.java
@@ -0,0 +1,40 @@
+package net.panayiotisgeorgiou.androidjavascript;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private BatteryManager battery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        // Find the charge Source
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING
+                || status == BatteryManager.BATTERY_STATUS_FULL;
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+        if (usbCharge) {
+            Toast.makeText(getApplicationContext(),
+                    "Mobile is charging on USB", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Mobile is charging on AC", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
