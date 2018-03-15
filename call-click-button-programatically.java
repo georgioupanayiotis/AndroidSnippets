be939b18f606dec9e36f534c167f3cf68c0be0c4
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2018-03-15 15:46:02 +0000

    Create call-click-button-programatically.java

---
+++ call-click-button-programatically.java
@@ -0,0 +1,9 @@
+//call a click on a button programatically
+Button btnClick = findViewById(R.id.btnClick);
+LinearLayout mLayout = findViewById(R.id.mLayout);
+  
+// perform click on button
+btnClick.performClick();
+  
+// perform click on Linear Layout
+mLayout.performClick();
